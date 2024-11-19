114968d78532cb88bb7e8e0cc0ebe6d8ff7b4916
vineas <alvienasyandika@gmail.com>
2024-11-19 11:16:00 +0700

    add neon css

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -17,37 +17,43 @@ const Hero = () => {
     "Next JS",
     "Express JS",
     "React Native",
+    "Adobe Illustrator",
+    "Adobe Photoshop",
+    "Adobe Premiere Pro",
+    "Capcut",
   ];
 
   return (
-    <div className="bg-gray-900">
-      <div className="  mx-auto container text-center md:text-left text-white h-screen items-center grid grid-cols-1 md:grid-cols-2">
-        <div className="mx-auto  ">
-          <h3 className="text-5xl text-center md:text-left font-bold mb-4">
-            Hi, {myName}
-          </h3>
-          <h3 className="text-3xl text-center md:text-left font-bold text-teal-500">
-            <Typewriter
-              options={{
-                strings: titles,
-                autoStart: true,
-                loop: true,
-                deleteSpeed: 50,
-                pauseFor: 1500,
-              }}
-            />
-          </h3>
-        </div>
+<div className="bg-gray-900">
+  <div className="mx-auto container text-center min-h-screen md:h-screen items-center md:text-left text-white grid grid-cols-1 md:grid-cols-2">
 
-        <div className="">
-          <Image
-            src={ava}
-            alt="alvienas"
-            className="w-2/3 mx-auto rounded-full"
-          />
-        </div>
-      </div>
+    <div className="p-4 md:p-0 md:order-2">
+      <Image
+        src={ava}
+        alt="alvienas"
+        className="md:w-2/3 md:mx-auto rounded-full"
+      />
     </div>
+
+    <div className="mx-auto md:order-1 ">
+      <h3 className="text-5xl md:text-5xl text-center font-bold">
+        Hi, {myName}
+      </h3>
+      <h3 className="text-3xl text-center md:text-left mb-60 md:mb-0 neon-text">
+        <Typewriter
+          options={{
+            strings: titles,
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 50,
+            pauseFor: 1500,
+          }}
+        />
+      </h3>
+    </div>
+  </div>
+</div>
+
   );
 };
 
