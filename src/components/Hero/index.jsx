c6037482f507ca77be19737257d985b54546562c
vineas <alvienasyandika@gmail.com>
2024-11-20 22:43:38 +0700

    edit hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,9 +2,15 @@ import Image from "next/image";
 import ava from "../../assets/img/ava.jpeg";
 import React from "react";
 import Typewriter from "typewriter-effect";
+import { VT323 } from "next/font/google";
+import { Inter } from "next/font/google";
+const vT = VT323({ subsets: ["latin"], weight: "400" });
+const inter = Inter({ subsets: ["latin"] });
 
 const Hero = () => {
-  const myName = "I'm Alvienas;";
+  const iAm = "I'm";
+  const myName = "Alvienas;";
+  const way = ">";
   const titles = [
     "Software Engineer",
     "Fullstack Developer",
@@ -24,35 +30,53 @@ const Hero = () => {
   ];
 
   return (
-<div className="bg-gray-900">
-  <div className="mx-auto container text-center min-h-screen md:h-screen items-center md:text-left text-white grid grid-cols-1 md:grid-cols-2">
-    <div className="p-4 md:p-0 md:order-2">
-      <Image
-        src={ava}
-        alt="alvienas"
-        className="md:w-2/3 md:mx-auto rounded-full"
-      />
-    </div>
+    <div className="bg-gray-900">
+      <div className="mx-auto container text-center min-h-screen md:h-screen items-center md:text-left text-white grid grid-cols-1 md:grid-cols-2">
+        <div className="p-4 md:p-0 md:order-2">
+          <Image
+            src={ava}
+            alt="alvienas"
+            className="md:w-2/3 md:mx-auto rounded-full"
+          />
+        </div>
 
-    <div className="mx-auto md:order-1 ">
-      <h3 className="text-4xl md:text-5xl text-center font-bold">
-        Hi, {myName}
-      </h3>
-      <h3 className="text-3xl text-center md:text-left mb-60 md:mb-0 neon-text">
-        <Typewriter
-          options={{
-            strings: titles,
-            autoStart: true,
-            loop: true,
-            deleteSpeed: 50,
-            pauseFor: 1500,
-          }}
-        />
-      </h3>
+        <div className="mx-auto md:order-1 ">
+          <div className={inter.className}>
+            <div className="flex">
+              <h3 className="text-4xl md:text-5xl text-center font-bold mr-3">
+                Hi,
+              </h3>
+              <h3 className="text-4xl md:text-5xl text-center font-bold mr-3">
+                {iAm}
+              </h3>
+              <h3 className="text-4xl md:text-5xl text-center font-bold neon-text">
+                {myName}
+              </h3>
+            </div>
+          </div>
+          <div className="flex">
+            <div className={vT.className}>
+              <h3 className="text-3xl md:text-4xl text-center md:text-left mb-60 md:mb-0 mr-2">
+                {way}
+              </h3>
+            </div>
+            <div className={vT.className}>
+              <h3 className="text-3xl md:text-4xl text-center md:text-left mb-60 md:mb-0">
+                <Typewriter
+                  options={{
+                    strings: titles,
+                    autoStart: true,
+                    loop: true,
+                    deleteSpeed: 50,
+                    pauseFor: 1500,
+                  }}
+                />
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
   );
 };
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
                 <div className='text-white w-11/12 md:container mx-auto py-4 flex justify-between items-center'>
                     <h2 className='text-3xl font-bold'><Link href={"/"}>VineasWeb</Link></h2>
                     <ul className='menu hidden md:flex bg-gray-900 '>
+                        <li className='px-7 py-1 hover:text-gray-500'><Link href={"#about"}>About</Link></li>
                         <li className='px-7 py-1 hover:text-gray-500'><Link href={"#services"}>Skills</Link></li>
                         <li className='px-7 py-1 hover:text-gray-500'><Link href={"#portofolio"}>Portofolio</Link></li>
-                        <li className='px-7 py-1 hover:text-gray-500'><Link href={"#about"}>About</Link></li>
                     </ul>
                 </div>
             </div>
