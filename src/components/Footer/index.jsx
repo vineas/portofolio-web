239f774fc91e43b2fab8ff5bd02b2d874b0667a7
vineas <alvienasyandika@gmail.com>
2024-11-24 23:43:43 +0700

    edit navbar

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,11 +10,11 @@ const Footer = () => {
               href="https://flowbite.com/"
               className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
             >
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
                 VineasWeb
               </span>
             </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 text-gray-400">
               <li>
                 <a href="#" className="hover:underline">
                   Contact
@@ -22,8 +22,8 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <hr className="my-6 border-gray-200 sm:mx-auto border-gray-700 lg:my-8" />
+          <span className="block text-sm text-gray-500 sm:text-center text-gray-400">
             © 2024{" "}
             <a href="https://flowbite.com/" className="hover:underline">
               VineasWeb
