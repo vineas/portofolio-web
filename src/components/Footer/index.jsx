abda771ea9b455deaccbcf0ef201d98072b92531
vineas <alvienasyandika@gmail.com>
2024-11-24 23:43:05 +0700

    edit navbar

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer className="shadow dark:bg-gray-900">
+      <footer className="shadow bg-gray-900">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <a
